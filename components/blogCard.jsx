c3becee2951230b473587e0c4b6df1f414289307
unknown <amirshaban53@gmail.com>
2022-04-26 07:01:12 +0300

    a.

--- components/blogCard.jsx
+++ components/blogCard.jsx
@@ -1,17 +1,17 @@
 import HTMLParser from "html-react-parser";
 import Link from "next/link";
 import classes from '../styles/blogCard.module.css';
+import Image from "next/image";
 
 const URL = process.env.NEXT_PUBLIC_URL;
 const BlogCard = ({id,title, description, author, image_URL, createdAt}) => {
     const date = createdAt.split('T')[0];
-    const imageURL = `${URL}/${image_URL}`
     
     return (
         <div className='col-12 col-sm-10 col-md-8 card mx-2 mb-3 p-0 shadow'>
             <div className="position-relative">
                 <div className= {classes.imageHeight}>
-                    <img src={imageURL} className={`card-img-top my-0`} />
+                    <img src={image_URL} className={`card-img-top my-0`} />
                 </div>
                 <p className={`${classes.date} bg-info text-light`}>{date}</p>
 
