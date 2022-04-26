2d6ade72535b6bfad368bdf79996dfa66b970ce3
unknown <amirshaban53@gmail.com>
2022-04-26 07:26:43 +0300

    a.

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,8 +1,7 @@
 import classes from '../styles/hero.module.css';
 const Hero = (props) => {
-    const bgImage = {backgroundImage: 'url("/layer 13.jpg")'}
     return(
-        <div className={classes.hero} style={bgImage}>
+        <div className={classes.hero}>
             {props.children}
         </div>  
     )
